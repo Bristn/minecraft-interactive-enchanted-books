fac6bcb19cda88faae3c968084bebdb0adc3271f
Bristn <Bristn@gmx.de>
2026-03-03 20:35:13 +0100

    Initial commit

---
+++ src/main/java/net/bristn/lectern/LecternEnchantedBooksClient.java
@@ -0,0 +1,11 @@
+package net.bristn.lectern;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LecternEnchantedBooksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+
+}
---
+++ src/main/java/net/bristn/lectern/LecternEnchantedBooksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bristn.lectern;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LecternEnchantedBooksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
